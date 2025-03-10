4d148d758fedcd7335000972b98135d4696dc5cb
YejiJeonn <yejijeon02@gmail.com>
2025-03-10 12:27:15 +0900

    Fix: header nav바 css 수정 및 nav바 페이지 연결

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import styles from '../../styles/Layout/Header.module.scss';
-import {Link} from "react-router-dom";
+import {Link, useNavigate} from "react-router-dom";
 
 const Header = () => {
+    const navigate = useNavigate();
 
     return (
         <header className={styles.headAll}>
             <p className={styles.title}>Being US</p>
 
             <nav className={styles.nav}>
-                <p>카테고리</p>
-                <p>진행방법</p>
-                <p>Q&A</p>
+                <p id={styles.category}>카테고리</p>
+                <p id={styles.tutorial} onClick={() => navigate("/tutorial")}>진행방법</p>
+                <p id={styles.qna} onClick={() => navigate("/qna")}>Q&A</p>
             </nav>
 
             <div className={styles.accountContainer}>
